e45eaad1a39295a63200afee09c2daff583c1c11
Vasyl Pyrig <pyrigok@i.ua>
2017-11-14 12:30:09 +0200

    dynamically filled table

--- students/views.py
+++ students/views.py
@@ -4,7 +4,21 @@ from django.http import HttpResponse
 # Views for Students
 
 def students_list (request):
-	return render (request, 'students/students_list.html', {})
+	students = (
+			{'id':1,
+			'first_name': u'Христина',
+			'last_name': u'Пиріг',
+			'ticket': 235,
+			'image': 'img/Khrs.jpg'},
+			
+			{'id': 2,
+			'first_name': u'Пиріг',
+			'last_name': u'Христинка',
+			'ticket': 564,
+			'image': 'img/Khrs.jpg'}
+		)
+	return render (request, 'students/students_list.html',
+				{'students': students})
 
 def students_add (request):
 	return HttpResponse ('<h1>Stud_add_form</h1>')
